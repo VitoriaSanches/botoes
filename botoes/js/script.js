a473e942ff172d6e36c326adfb9403e44caf8e76
Vitória Ferreira Sanches <106158486+VitoriaSanches@users.noreply.github.com>
2023-05-20 10:59:05 -0300

    Add files via upload

---
+++ botoes/js/script.js
@@ -0,0 +1,11 @@
+document.getElementById("botao_vermelho").addEventListener("click", function() {
+    document.body.style.backgroundColor = "red";
+});
+
+document.getElementById("botao_verde").addEventListener("click", function() {
+    document.body.style.backgroundColor = "green";
+});
+
+document.getElementById("botao_azul").addEventListener("click", function() {
+    document.body.style.backgroundColor = "blue";
+});
